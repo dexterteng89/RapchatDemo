8e491c9b35166ed8c2e92c32c11028b31b4fd15c
Dexter Teng <kpib.lol@gmail.com>
2013-05-06 14:09:24 -0500

    removing SC login

--- BattleRap/BRLoginViewController.m
+++ BattleRap/BRLoginViewController.m
@@ -8,7 +8,6 @@
 
 #import "BRLoginViewController.h"
 #import "BRBattleStore.h"
-#import "SCUI.h"
 
 @interface BRLoginViewController ()
 {
@@ -77,27 +76,6 @@
 
 }
 
-- (IBAction) loginSC:(id) sender
-{
-    SCLoginViewControllerCompletionHandler handler = ^(NSError *error) {
-        if (SC_CANCELED(error)) {
-            NSLog(@"Canceled!");
-        } else if (error) {
-            NSLog(@"Error: %@", [error localizedDescription]);
-        } else {
-            NSLog(@"Done!");
-        }
-    };
-    
-    [SCSoundCloud requestAccessWithPreparedAuthorizationURLHandler:^(NSURL *preparedURL) {
-        SCLoginViewController *loginViewController;
-        
-        loginViewController = [SCLoginViewController
-                               loginViewControllerWithPreparedURL:preparedURL
-                               completionHandler:handler];
-        [self presentModalViewController:loginViewController animated:YES];
-    }];
-}
 
 #pragma mark - UITextViewDelegate methods
 
