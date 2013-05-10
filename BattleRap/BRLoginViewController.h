90254ec8bc6dca8bda8cf3f46809e66fb55a71a6
henwen <hwdearborn@gmail.com>
2013-05-10 15:49:46 -0500

    LoginView now toggles btw signup and sign in mode; added signup/in form validation

--- BattleRap/BRLoginViewController.h
+++ BattleRap/BRLoginViewController.h
@@ -14,7 +14,13 @@
 @property (weak, nonatomic) IBOutlet UITextField *usernameField;
 @property (weak, nonatomic) IBOutlet UITextField *emailField;
 @property (weak, nonatomic) IBOutlet UITextField *passwordField;
+@property (weak, nonatomic) IBOutlet UIButton *footerButton;
+@property (weak, nonatomic) IBOutlet UILabel *emailLabel;
+@property (weak, nonatomic) IBOutlet UILabel *passwordLabel;
+@property (weak, nonatomic) IBOutlet UILabel *usernameLabel;
 
-- (IBAction)login:(id)sender;
+- (void)signUp;
+- (void)signIn;
+- (IBAction)toggleSignupMode:(id)sender;
 
 @end
--- BattleRap/BRLoginViewController.m
+++ BattleRap/BRLoginViewController.m
@@ -10,19 +10,26 @@
 #import "BRBattleStore.h"
 #import "BRHTTPClient.h"
 
-#define kOFFSET_FOR_KEYBOARD 130.0
+#define kOFFSET_FOR_KEYBOARD 105.0
 
 @interface BRLoginViewController ()
 {
     NSArray *userArray;
-    BOOL _formValid;
+    BOOL _loginAttemped;
+    BOOL _signUpMode;
 }
-- (BOOL)validateForm;
+- (BOOL)validateFormFields;
+- (void)validateAndLogin;
+- (void)toggleEmailFieldHidden;
+- (void)keyboardWillShow;
+- (void)keyboardWillHide;
+- (void)setSlideViewUp:(BOOL)movedUp;
 
 @end
 
 @implementation BRLoginViewController
-@synthesize usernameField, passwordField, emailField;
+@synthesize usernameField, passwordField, emailField, footerButton,
+            passwordLabel, emailLabel, usernameLabel;
 
 #pragma mark - UIViewController
 
@@ -40,7 +47,8 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-    _formValid = NO;
+    
+    _signUpMode = YES;
 }
 
 - (void)viewWillAppear:(BOOL)animated
@@ -78,16 +86,117 @@
 
 #pragma mark - Actions
 
-- (IBAction)login:(id)sender {
-    
-    // Validate login
+- (void)signUp
+{
+    NSLog(@"SignUp called");
     
     // TODO: progress HUD
     
-//    [[BRHTTPClient sharedClient] 
+    [[BRHTTPClient sharedClient] signUpWithHandle:usernameField.text
+      email:emailField.text
+      password:passwordField.text
+      success:^(AFJSONRequestOperation *operation, id responseObject) {
+        //TODO: dismiss HUD on success
+                                              
+        [self.navigationController dismissViewControllerAnimated:YES completion:nil];
+        
+    } failure:^(AFJSONRequestOperation *operation, NSError *error) {
+        
+        //TODO: update HUD to show failure
+        
+        NSString *message = [[operation responseJSON] objectForKey:@"error_description"];
+        
+        UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"Error"
+                                                        message:message
+                                                       delegate:nil
+                                              cancelButtonTitle:@"OK"
+                                              otherButtonTitles:nil];
+        [alert show];        
+    }];
+}
+
+- (void)signIn
+{
+    NSLog(@"SignIn called");
     
-    [self.presentingViewController dismissViewControllerAnimated:YES
-                                                      completion:nil];
+    // TODO: progress HUD
+    
+    [[BRHTTPClient sharedClient] signInWithHandle:usernameField.text password:passwordField.text success:^(AFJSONRequestOperation *operation, id responseObject) {
+        //TODO: dismiss HUD on success
+        
+        [self.navigationController dismissViewControllerAnimated:YES completion:nil];
+    } failure:^(AFJSONRequestOperation *operation, NSError *error) {
+        NSString *message = [[operation responseJSON] objectForKey:@"error_description"];
+        
+        UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"Error"
+                                                        message:message
+                                                       delegate:nil
+                                              cancelButtonTitle:@"OK"
+                                              otherButtonTitles:nil];
+        [alert show];
+    }];
+}
+
+
+- (IBAction)toggleSignupMode:(id)sender
+{
+    if (_signUpMode) {
+        [self toggleEmailFieldHidden];
+        
+        [footerButton setTitle:@"DON'T HAVE AN ACCOUNT?" forState:UIControlStateNormal];
+        usernameLabel.text = @"ENTER YOUR RAPPER NAME";
+        
+        _signUpMode = NO;
+    } else {
+        [self toggleEmailFieldHidden];
+        
+        [footerButton setTitle:@"ALREADY HAVE AN ACCOUNT?" forState:UIControlStateNormal];
+        usernameLabel.text = @"CREATE YOUR RAPPER NAME";
+        
+        _signUpMode = YES;
+    }
+    
+}
+
+- (void)toggleEmailFieldHidden
+{
+    if (_signUpMode) {
+        // hide email field and slide password field up
+        
+        [UIView animateWithDuration:0.25f animations:^{
+            emailField.alpha = 0;
+            emailLabel.alpha = 0;
+            
+            CGRect passFieldFrame = passwordField.frame;
+            passFieldFrame.origin.y -= 76.0;
+            passwordField.frame = passFieldFrame;
+            
+            CGRect passLabelFrame = passwordLabel.frame;
+            passLabelFrame.origin.y -= 76.0;
+            passwordLabel.frame = passLabelFrame;
+            
+        } completion:nil];
+        [UIView commitAnimations];
+    } else {
+        // show email field and move password field down
+        [UIView animateWithDuration:0.25f animations:^{
+            CGRect passFieldFrame = passwordField.frame;
+            passFieldFrame.origin.y += 76.0;
+            passwordField.frame = passFieldFrame;
+            
+            CGRect passLabelFrame = passwordLabel.frame;
+            passLabelFrame.origin.y += 76.0;
+            passwordLabel.frame = passLabelFrame;
+            
+        } completion:^(BOOL finished) {
+            [UIView animateWithDuration:0.05f animations:^{
+                emailField.alpha = 1;
+                emailLabel.alpha = 1;
+            }];
+        }];
+        [UIView commitAnimations];
+        
+    }
 }
 
 - (void)postUserInformation
@@ -120,51 +229,79 @@
 
 - (BOOL)textFieldShouldReturn:(UITextField *)textField
 {
+    // If form empty, return key will jump to next field. If login was already 
+    // attempted and failed, return key will become "GO" button for all fields
+    
     if (textField == self.usernameField) {
-        [self.emailField becomeFirstResponder];
-    } else if (textField == self.emailField) {
-        [self.passwordField becomeFirstResponder];
-    } else if (textField == self.passwordField) {
-        if ([self validateForm]) {
-            NSLog(@"Form Valid");
-            //log in
+        if (!_loginAttemped) {
+            if (_signUpMode)
+                [self.emailField becomeFirstResponder];
+            else
+                [self.passwordField becomeFirstResponder];
         } else {
-            NSLog(@"Form NOT Valid");
-            //pop alert
+            [self validateAndLogin];
         }
+    } else if (textField == self.emailField) {
+        if (!_loginAttemped) 
+            [self.passwordField becomeFirstResponder];
+        else
+            [self validateAndLogin];
+    } else if (textField == self.passwordField) {
+        [self validateAndLogin];
     }
     
     return NO;
 }
 
-//- (BOOL)textField:(UITextField *)textField
-//    shouldChangeCharactersInRange:(NSRange)range
-//                replacementString:(NSString *)string
-//{
-//    //When entering password, validate form
-//    if (textField == self.passwordField) {
-//        dispatch_async(dispatch_get_main_queue(), ^{
-//            [self validateForm];
-//        });
-//    }
-//	return YES;
-//}
-
-- (BOOL)validateForm
+- (void)validateAndLogin
 {
-    BOOL handleAndPasswordValid = usernameField.text.length >= 1 &&
-                                  passwordField.text.length >= 6;
-    NSString *emailRegex = @".+@.+\\.[A-Za-z]{2}[A-Za-z]*";
-    NSPredicate *emailTest = [NSPredicate predicateWithFormat:@"SELF MATCHES %@", emailRegex];
-    BOOL emailValid = [emailTest evaluateWithObject:emailField.text];
-    
-    if (handleAndPasswordValid && emailValid) {
-        return YES;
+    if ([self validateFormFields]) {
+        NSLog(@"Form Valid");
+        //log in
+        if (_signUpMode) {
+            [self signUp];
+        } else {
+            [self signIn];
+        }
+        
     } else {
-        return NO;
+        NSLog(@"Form NOT Valid");
+        UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"Something's wrong!"
+                                                        message:@"RULES: Name must be 3-15 characters, no spaces. Email must be valid. Password must be more than 5 characters"
+                                                       delegate:self
+                                              cancelButtonTitle:nil
+                                              otherButtonTitles:@"OK", nil];
+        [alert show];
+        _loginAttemped = YES;
+        self.emailField.returnKeyType = UIReturnKeyGo;
+        self.usernameField.returnKeyType = UIReturnKeyGo;
     }
 }
 
+- (BOOL)validateFormFields
+{
+    BOOL handleValid, passwordValid, emailValid;
+    
+    NSString *handleRegex = @"[a-zA-Z0-9_]{3,15}";
+    NSPredicate *handleTest = [NSPredicate predicateWithFormat:@"SELF MATCHES %@", handleRegex];
+    handleValid = [handleTest evaluateWithObject: usernameField.text];
+
+    NSString *emailRegex = @".+@.+\\.[A-Za-z]{2}[A-Za-z]*";
+    NSPredicate *emailTest = [NSPredicate predicateWithFormat:@"SELF MATCHES %@", emailRegex];
+    emailValid = [emailTest evaluateWithObject: emailField.text];
+
+    passwordValid = passwordField.text.length >= 6;
+    
+    if (_signUpMode) {
+        if (handleValid && emailValid && passwordValid) 
+            return YES;
+    } else {
+        if (handleValid && passwordValid)
+            return YES;
+    }
+    
+    return NO;
+}
 
 #pragma mark - Translate View
 
@@ -174,11 +311,11 @@
 {
     if (self.view.frame.origin.y >= 0)
     {
-        [self setViewMovedUp:YES];
+        [self setSlideViewUp:YES];
     }
     else if (self.view.frame.origin.y < 0)
     {
-        [self setViewMovedUp:NO];
+        [self setSlideViewUp:NO];
     }
 }
 
@@ -186,18 +323,18 @@
 {
     if (self.view.frame.origin.y >= 0)
     {
-        [self setViewMovedUp:YES];
+        [self setSlideViewUp:YES];
     }
     else if (self.view.frame.origin.y < 0)
     {
-        [self setViewMovedUp:NO];
+        [self setSlideViewUp:NO];
     }
 }
 
--(void)setViewMovedUp:(BOOL)movedUp
+-(void)setSlideViewUp:(BOOL)movedUp
 {
     [UIView beginAnimations:nil context:NULL];
-    [UIView setAnimationDuration:0.25]; // if you want to slide up the view
+    [UIView setAnimationDuration:0.2]; // if you want to slide up the view
     
     CGRect rect = self.view.frame;
     if (movedUp)
